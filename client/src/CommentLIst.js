c77dac3c9f595bfdfb8b6204e7632a039d98311c
Serge <spnikit@gmail.com>
2020-05-08 20:43:19 +0300

    client add comment status info

--- client/src/CommentLIst.js
+++ client/src/CommentLIst.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 function CommentList({ comments }) {
-  const renderedComments = comments.map(({ id, content }) => {
-    return <li key={id}>{content}</li>;
+  const renderedComments = comments.map(({ id, content, status }) => {
+    let contentToRender;
+    switch (status) {
+      case "approved":
+        contentToRender = content;
+        break;
+      case "pending":
+        contentToRender = "This comment is awaiting moderation";
+        break;
+      case "rejected":
+      default:
+        contentToRender = "This comment has been rejected";
+        break;
+    }
+
+    return <li key={id}>{contentToRender}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
