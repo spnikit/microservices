c68ce81490f15bc2ea091ddd8b40263c62e9d73f
Serge <spnikit@gmail.com>
2020-05-08 22:32:48 +0300

    add event-bus saving events functionality and applying it to query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,15 +5,23 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", async (req, res) => {
+const events = [];
+
+app.post("/events", (req, res) => {
   const event = req.body;
 
-  await axios.post("http://localhost:6001/events", event); // comments service
-  await axios.post("http://localhost:4000/events", event); // posts service
-  await axios.post("http://localhost:6002/events", event); // query service
-  await axios.post("http://localhost:4003/events", event); // moderation service
+  events.push(event);
+
+  axios.post("http://localhost:6001/events", event); // comments service
+  axios.post("http://localhost:4000/events", event); // posts service
+  axios.post("http://localhost:6002/events", event); // query service
+  axios.post("http://localhost:4003/events", event); // moderation service
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => console.log("Server is listening on PORT 4005"));
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 // ******** SERVER AND MIDDLEWARE SETUP *********
 app.use(bodyParser.json());
@@ -17,6 +18,26 @@ app.get("/posts", (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
+  handleEvent(type, data);
+
+  res.send({});
+});
+
+// ******** SERVER FIRE UP *********
+app.listen(6002, async () => {
+  console.log("Server is listening on PORT 6002");
+
+  const resp = await axios.get("http://localhost:4005/events");
+
+  for (const { type, data } of resp.data) {
+    console.log("Processing event", type);
+    handleEvent(type, data);
+  }
+});
+
+// ******** HELPER FUNCTIONS *********
+
+function handleEvent(type, data) {
   const eventType = type.trim().toLowerCase();
   switch (eventType) {
     case "postcreated": {
@@ -24,28 +45,18 @@ app.post("/events", (req, res) => {
       posts[id] = { id, title, comments: [] };
       break;
     }
-
     case "commentcreated": {
       const { id, content, postId, status } = data;
       posts[postId].comments.push({ id, content, status });
       break;
     }
-
     case "commentupdated": {
       const { postId, id, content, status } = data;
-
       const comments = posts[postId].comments;
       const comment = comments.find((comment) => comment.id === id);
-
       comment.status = status;
       comment.content = content;
-
       break;
     }
   }
-
-  res.send({});
-});
-
-// ******** SERVER FIRE UP *********
-app.listen(6002, () => console.log("Server is listening on PORT 6002"));
+}
